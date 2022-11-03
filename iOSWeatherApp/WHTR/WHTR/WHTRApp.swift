f3b75a1bda9e9e3863f2c9290faff3ba7f0f9aaa
foal20ym <93208443+foal20ym@users.noreply.github.com>
2022-11-03 20:17:32 +0100

    Add files via upload

---
+++ iOSWeatherApp/WHTR/WHTR/WHTRApp.swift
@@ -0,0 +1,17 @@
+//
+//  WHTRApp.swift
+//  WHTR
+//
+//  Created by Alexander Forsanker on 11/2/22.
+//
+
+import SwiftUI
+
+@main
+struct WHTRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
