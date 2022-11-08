5c37e62f798973b6e4859e590e29e9de6d617978
foal20ym <alexander-forsanker2011@hotmail.com>
2022-11-08 11:52:08 +0100

    Added the feature to show what type of weather it is.

--- iOSWeatherApp/WHTR/WHTR/Views/WeatherView.swift
+++ iOSWeatherApp/WHTR/WHTR/Views/WeatherView.swift
@@ -10,25 +10,33 @@ import SwiftUI
 
 struct WeatherView: View {
     var weather: ResponseBody
+    var weatherCodeDictionary =
+    [
+        0:"Clear", 1:"Mainly clear", 2:"Partly cloudy",
+        3:"Overcast", 45:"Fog", 48:"Thick fog",
+        51:"Light drizzle", 53:"Moderate drizzle",55:"Dense drizzle",
+        56:"Light freezing drizzle", 57:"Dense freezing drizzle",61:"Light rain fall",
+        63:"Moderate rain fall", 65:"Heavy rain fall",66:"Light freezing rain",
+        67:"Heavy freezing rain", 71:"Light snow fall",73:"Moderate snow fall",
+        75:"Heavy snow fall", 77:"Snow",80:"Slight rain showers",
+        81:"Heavy rain showers", 82:"Violent rain showers",85:"Moderate snow showers",
+        86:"Heavy snow showers", 95:"Thunderstorm",96:"Thunderstorm with slight hail", 99:"Thunderstorm with heavy hail"
+    ]
+
     
     var body: some View {
         ZStack(alignment: .leading) {
             VStack {
                 VStack(alignment: .leading, spacing: 5) {
-                    //let tempVarToInt = Int(ceil(weather.hourly.temperature_2m[0]))
                     
                     Text("Timezone: \(weather.timezone)")
                         .bold().font(.title)
-                    
                     Text("Today, \(Date().formatted(.dateTime.month().day().hour().minute()))")
                         .fontWeight(.light)
                     
-                    //Text("\(tempVarToInt)")
                 }
                 .frame(maxWidth: .infinity, alignment: .leading)
                 
-                Spacer()
-                //Text("Latitude: \(weather.latitude), Longitude: \(weather.longitude)")
                 Spacer()
                 
                 VStack {
@@ -37,17 +45,18 @@ struct WeatherView: View {
                             Image(systemName: "sun.max")
                                 .font(.system(size: 40))
                             
-                            Text("Clear")
+                            Text("\(weatherCodeDictionary[weather.current_weather.weathercode] ?? "Clear")")
                         }
                         .frame(width: 150, alignment: .leading)
                         
                         Spacer()
                         
-                        let tempVarToInt = Int(weather.hourly.temperature_2m[0])
+                        let tempVarToInt = Int(weather.current_weather.temperature)
                         Text("\(tempVarToInt)\(weather.hourly_units.temperature_2m)")
                             .font(.system(size: 60))
                             .fontWeight(.bold)
                             .padding()
+                
                     }
                     
                     Spacer()
@@ -109,6 +118,9 @@ struct WeatherView: View {
         .background(Color(hue: 0.649, saturation: 0.884, brightness: 0.561))
         .preferredColorScheme(.dark)
     }
+    func wc(inputWC: Int){
+        print("\(weatherCodeDictionary[inputWC] ?? "Clear")")
+    }
 }
 
 struct WeatherView_Previews: PreviewProvider {
@@ -116,3 +128,4 @@ struct WeatherView_Previews: PreviewProvider {
         WeatherView(weather: previewWeather)
     }
 }
+
